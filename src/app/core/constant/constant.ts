178596e312b99c568e7d0a8a68917c3d0028f142
MuhammadHashim06 <muhammad.hashimsid@gmail.com>
2024-08-02 13:57:24 +0500

    Updated angular.json: added fileReplacements in development configuration and analytics in cli section

---
+++ src/app/core/constant/constant.ts
@@ -0,0 +1,12 @@
+export const constant={
+    apiUrl:'http://localhost:4200/home',
+    login:{
+        success:{
+            code:200,
+            message:'Login Success',
+        },fail:{
+            code:400,
+            message:'Login Fail',
+        }
+    }
+}
